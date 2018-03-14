4222bdfe3cf2a6f055711dbac617f468b5ec2ebf
bumper86 <szczstaw@o2.pl>
2018-03-14 21:11:31 +0100

    Nauka baz daych w javie poprzez aplikacje REST

---
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -0,0 +1,23 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DbService {
+    @Autowired
+    private TaskRepository repository;
+
+    public List<Task> getAllTasks() {
+        return repository.findAll();
+    }
+
+    public Task getIdTask(String id) {
+        return repository.findAll().get(Integer.parseInt(id));
+    }
+}
